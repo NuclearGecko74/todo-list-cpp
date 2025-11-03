94920897c5452e208a7d35d8de42fa998de73694
Anton Olguin <antonolguin1@gmail.com>
2025-11-02 20:13:49 -0600

    Create App Class

---
+++ include/App.h
@@ -0,0 +1,12 @@
+﻿//
+// Created by user on 02/11/2025.
+//
+
+#ifndef TODOLIST_APP_H
+#define TODOLIST_APP_H
+
+class App {
+
+};
+
+#endif //TODOLIST_APP_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,7 @@
-#include "raylib.h"
+#include "App.h"
 
 int main()
 {
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "To-Do List");
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Toy chica es la unica animatronico, la cual lleva ropa la cual es un short, que la hace ver muy SECY", 0, 200, 20, BLACK);
-
-        EndDrawing();
-    }
-    CloseWindow();
 
     return 0;
 }
