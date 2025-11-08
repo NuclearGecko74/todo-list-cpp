ac1d282c9ecae00a24189211be719c36239258e0
Anton Olguin <antonolguin1@gmail.com>
2025-11-08 15:32:14 -0600

    Add Window class

---
+++ Core/Source/Core/Window.cpp
@@ -0,0 +1,40 @@
+﻿#include "Window.h"
+
+#include <iostream>
+
+namespace Core {
+    Window::Window(const WindowSpecification &specification)
+        : m_Specification(specification)
+    {
+        uint32_t flags = 0;
+        if (m_Specification.IsResizeable)
+            flags |= FLAG_WINDOW_RESIZABLE;
+        if (m_Specification.VSync)
+            flags |= FLAG_VSYNC_HINT;
+
+        if (flags != 0)
+            SetConfigFlags(flags);
+
+        InitWindow(m_Specification.Width, m_Specification.Height, m_Specification.Title.c_str());
+
+        SetTargetFPS(60);
+
+        std::cout << "Raylib window initialized.\n";
+    }
+
+    Window::~Window()
+    {
+        CloseWindow();
+        std::cout << "Raylib window closed.\n";
+    }
+
+    Vector2 Window::GetSize()
+    {
+        return { static_cast<float>(GetScreenWidth()), static_cast<float>(GetScreenHeight()) };
+    }
+
+    bool Window::ShouldClose() const
+    {
+        return WindowShouldClose();
+    }
+}
---
+++ Core/Source/Core/Window.h
@@ -0,0 +1,36 @@
+﻿//
+// Created by user on 08/11/2025.
+//
+
+#ifndef TODOLIST_WINDOW_H
+#define TODOLIST_WINDOW_H
+
+#include <string>
+
+#include "raylib.h"
+
+namespace Core {
+    struct WindowSpecification {
+        std::string Title;
+        uint32_t Width = 1280;
+        uint32_t Height = 720;
+        bool IsResizeable = true;
+        bool VSync = true;
+    };
+
+    class Window {
+    public:
+        Window(const WindowSpecification& specification = WindowSpecification());
+        ~Window();
+
+        Vector2 GetSize();
+
+        bool ShouldClose() const;
+    private:
+        WindowSpecification m_Specification;
+    };
+}
+
+
+
+#endif
