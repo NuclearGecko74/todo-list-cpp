1c20fd6fa987c9dbaf8547911c5007af4b170c57
Anton Olguin <antonolguin1@gmail.com>
2025-11-08 19:13:34 -0600

    Add Task and TaskList classes

---
+++ App/Source/Model/Task.cpp
@@ -0,0 +1,29 @@
+﻿#include "Task.h"
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <string>
+
+void Task::setDueDate(int year, int month, int day, int hour, int minute, int second)
+{
+    std::tm timeStruct = {};
+    timeStruct.tm_year = year - 1900; // years since 1900
+    timeStruct.tm_mon = month - 1;    // 0 = january
+    timeStruct.tm_mday = day;
+    timeStruct.tm_hour = hour;
+    timeStruct.tm_min = minute;
+    timeStruct.tm_sec = second;
+
+    std::time_t timeValue = std::mktime(&timeStruct);
+    m_dueDate = std::chrono::system_clock::from_time_t(timeValue);
+}
+
+// Returns date and time using ctime
+std::string Task::getDueDate() const
+{
+    std::time_t timeValue = std::chrono::system_clock::to_time_t(m_dueDate);
+    char buffer[26]; // ctime_s needs a buffer of at least 26 characters
+    ctime_s(buffer, sizeof(buffer), &timeValue);
+
+    return buffer;
+}
---
+++ App/Source/Model/TaskList.h
@@ -0,0 +1,35 @@
+﻿#ifndef TODOLIST_TASKLIST_H
+#define TODOLIST_TASKLIST_H
+
+#include <vector>
+#include "Task.h"
+
+class TaskList {
+private:
+    std::string m_name;
+    std::string m_description;
+    std::vector<Task> m_List;
+
+public:
+    // constructors
+    TaskList() = delete;
+
+    explicit TaskList(const std::string& name)
+        : m_name(name) {};
+
+    explicit TaskList(const std::string& name, const std::string& description)
+        :  m_name(name), m_description(description) {};
+
+    explicit TaskList(const std::string& name, const Task& task);
+
+    explicit TaskList(const std::string& name, const std::string& description, const Task& task);
+
+    // setters
+    inline void setName(const std::string& name) { m_name = name; }
+
+    // getters
+    [[nodiscard]] inline std::string getName() const { return m_name; }
+    [[nodiscard]] inline size_t getSize() const { return m_List.size(); }
+};
+
+#endif
