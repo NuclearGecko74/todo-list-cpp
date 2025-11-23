1acffa13065b74c6d5b251f5c74ef4040b2d015a
Anton Olguin <antonolguin1@gmail.com>
2025-11-23 15:37:52 -0600

    Change unique_ptr to shared_ptr

--- Core/Source/Core/Layer.cpp
+++ Core/Source/Core/Layer.cpp
@@ -6,19 +6,20 @@ namespace Core {
 
     void Layer::QueueTransition(std::unique_ptr<Layer> toLayer)
     {
-        Application::Get().SubmitPostFrameAction([this, newLayer = std::move(toLayer)]() mutable {
+        auto pendingLayerWrapper = std::make_shared<std::unique_ptr<Layer>>(std::move(toLayer));
 
-            auto& layerStack = Application::Get().GetLayerStack();
+        Application::Get().SubmitPostFrameAction([this, pendingLayerWrapper]() {
+
+            auto& layerStack = Application::Get().m_LayerStack;
 
             for (auto& layer : layerStack)
             {
                 if (layer.get() == this)
                 {
-                    layer = std::move(newLayer);
+                    layer = std::move(*pendingLayerWrapper);
                     return;
                 }
             }
         });
     }
-
 }
