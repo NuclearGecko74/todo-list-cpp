aec51e4a11505ef3fa1011a4d5736a66995c0eab
Anton Olguin <antonolguin1@gmail.com>
2025-11-23 12:51:47 -0600

    Remove RizzList namespace

--- App/Source/UI/Theme.h
+++ App/Source/UI/Theme.h
@@ -3,20 +3,16 @@
 
 #include "raylib.h"
 
-//TODO Load Theme from Settings
+// TODO Load Theme from Settings
 
-namespace RizzList {
-    struct Theme {
-        // App Color Palette
-        static constexpr Color BG_Sidebar {30, 30, 47, 255};
-        static constexpr Color BG_Main {200, 179, 255, 255};
-        static constexpr Color Border_Panel {78, 78, 110, 255};
+struct Theme {
+    // App Color Palette
+    static constexpr Color BG_Sidebar {30, 30, 47, 255};
+    static constexpr Color BG_Main {200, 179, 255, 255};
+    static constexpr Color Border_Panel {78, 78, 110, 255};
+    static constexpr Color BG_Panel {255, 255, 255, 255};
+    static constexpr  Color Text_Light {255, 255, 255, 255};
 
-        static constexpr Color BG_Panel {255, 255, 255, 255};
-
-        static constexpr  Color Text_Light {255, 255, 255, 255};
-
-    };
-}
+};
 
 #endif
