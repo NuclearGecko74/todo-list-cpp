dcb333028d031754607818c1fb52434d3316a2d4
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2025-12-06 20:00:59 -0600

    Add database managers to layers

--- App/Source/AppResources.h
+++ App/Source/AppResources.h
@@ -2,15 +2,20 @@
 #define APPRESOURCES_H
 
 #include "raylib.h"
+#include <SQLiteCpp/SQLiteCpp.h>
 
 class AppResources {
 public:
     static void Init();
     static void Shutdown();
     static Font GetFont();
+    static SQLite::Database& GetDatabase();
 
 private:
     static Font s_Font;
+    static std::unique_ptr<SQLite::Database> s_Database;
+
+    static void InitDatabase();
 };
 
 
